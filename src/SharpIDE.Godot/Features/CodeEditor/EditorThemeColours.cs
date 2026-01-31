ac9462586db8f4bcca72d22d07ac588f76cfc644
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-01-31 14:23:18 +1000

    refactor v2

--- src/SharpIDE.Godot/Features/CodeEditor/EditorThemeColours.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/EditorThemeColours.cs
@@ -6,23 +6,23 @@ public static class EditorThemeColours
 {
     public static readonly EditorThemeColorSet Light = new EditorThemeColorSet
     {
-        Orange = CachedColorsLight.Orange,
-        White = CachedColorsLight.White,
-        Yellow = CachedColorsLight.Yellow,
-        CommentGreen = CachedColorsLight.CommentGreen,
-        KeywordBlue = CachedColorsLight.KeywordBlue,
-        LightOrangeBrown = CachedColorsLight.LightOrangeBrown,
-        NumberGreen = CachedColorsLight.NumberGreen,
-        InterfaceGreen = CachedColorsLight.InterfaceGreen,
-        ClassGreen = CachedColorsLight.ClassGreen,
-        VariableBlue = CachedColorsLight.VariableBlue,
-        Gray = CachedColorsLight.Gray,
-        Pink = CachedColorsLight.Pink,
-        ErrorRed = CachedColorsLight.ErrorRed,
+        Orange = TextEditorDotnetColoursLight.Orange,
+        White = TextEditorDotnetColoursLight.White,
+        Yellow = TextEditorDotnetColoursLight.Yellow,
+        CommentGreen = TextEditorDotnetColoursLight.CommentGreen,
+        KeywordBlue = TextEditorDotnetColoursLight.KeywordBlue,
+        LightOrangeBrown = TextEditorDotnetColoursLight.LightOrangeBrown,
+        NumberGreen = TextEditorDotnetColoursLight.NumberGreen,
+        InterfaceGreen = TextEditorDotnetColoursLight.InterfaceGreen,
+        ClassGreen = TextEditorDotnetColoursLight.ClassGreen,
+        VariableBlue = TextEditorDotnetColoursLight.VariableBlue,
+        Gray = TextEditorDotnetColoursLight.Gray,
+        Pink = TextEditorDotnetColoursLight.Pink,
+        ErrorRed = TextEditorDotnetColoursLight.ErrorRed,
         
-        RazorComponentGreen = CachedColorsLight.RazorComponentGreen,
-        RazorMetaCodePurple = CachedColorsLight.RazorMetaCodePurple,
-        HtmlDelimiterGray = CachedColorsLight.HtmlDelimiterGray
+        RazorComponentGreen = TextEditorDotnetColoursLight.RazorComponentGreen,
+        RazorMetaCodePurple = TextEditorDotnetColoursLight.RazorMetaCodePurple,
+        HtmlDelimiterGray = TextEditorDotnetColoursLight.HtmlDelimiterGray
     };
     
     public static readonly EditorThemeColorSet Dark = new EditorThemeColorSet
@@ -66,4 +66,46 @@ public class EditorThemeColorSet
     public required Color RazorComponentGreen;
     public required Color RazorMetaCodePurple;
     public required Color HtmlDelimiterGray;
+}
+
+public static class CachedColors
+{
+    public static readonly Color Orange = new("f27718");
+    public static readonly Color White = new("dcdcdc");
+    public static readonly Color Yellow = new("dcdcaa");
+    public static readonly Color CommentGreen = new("57a64a");
+    public static readonly Color KeywordBlue = new("569cd6");
+    public static readonly Color LightOrangeBrown = new("d69d85");
+    public static readonly Color NumberGreen = new("b5cea8");
+    public static readonly Color InterfaceGreen = new("b8d7a3");
+    public static readonly Color ClassGreen = new("4ec9b0");
+    public static readonly Color VariableBlue = new("9cdcfe");
+    public static readonly Color Gray = new("a9a9a9");
+    public static readonly Color Pink = new("c586c0");
+    public static readonly Color ErrorRed = new("da5b5a");
+    
+    public static readonly Color RazorComponentGreen = new("0b7f7f");
+    public static readonly Color RazorMetaCodePurple = new("a699e6");
+    public static readonly Color HtmlDelimiterGray = new("808080");
+}
+
+public static class TextEditorDotnetColoursLight
+{
+    public static readonly Color Orange = new("b776fb"); //
+    public static readonly Color White = new("000000"); //
+    public static readonly Color Yellow = new("74531f"); //
+    public static readonly Color CommentGreen = new("008000"); //
+    public static readonly Color KeywordBlue = new("0000ff"); //
+    public static readonly Color LightOrangeBrown = new("a31515"); //
+    public static readonly Color NumberGreen = new("000000"); //
+    public static readonly Color InterfaceGreen = new("2b91af"); //
+    public static readonly Color ClassGreen = new("2b91af"); //
+    public static readonly Color VariableBlue = new("1f377f"); //
+    public static readonly Color Gray = new("a9a9a9"); //
+    public static readonly Color Pink = new("c586c0"); //
+    public static readonly Color ErrorRed = new("da5b5a"); //
+    
+    public static readonly Color RazorComponentGreen = new("0b7f7f");
+    public static readonly Color RazorMetaCodePurple = new("826ee6");
+    public static readonly Color HtmlDelimiterGray = new("808080");
 }
